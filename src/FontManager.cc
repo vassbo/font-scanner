b4d0331e67f7e07c9dd60123d8f23d03b1fe8ca5
Kristoffer <kristoffervassbo@gmail.com>
2025-05-22 12:32:57 +0200

    Fix issue

--- src/FontManager.cc
+++ src/FontManager.cc
@@ -23,19 +23,10 @@ Napi::Array collectResults(Napi::Env env, ResultSet *results) {
 // converts a FontDescriptor to a JavaScript object
 Napi::Value wrapResult(Napi::Env env, FontDescriptor *result) {
   Napi::EscapableHandleScope scope(env);
-  if (result == NULL) {
+  if (result == NULL)
     return scope.Escape(env.Null());
-  }
 
-  Napi::Object res;
-  try {
-    res = result->toJSObject(env);
-  } catch (...) {
-    delete result;
-    Napi::Error::New(env, "Failed to convert FontDescriptor to JS object").ThrowAsJavaScriptException();
-    return env.Null();
-  }
-
+  Napi::Object res = result->toJSObject(env);
   delete result;
   return scope.Escape(res);
 }
